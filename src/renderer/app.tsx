c64d96c29498fe672a56f2d98306d1230a8e643a
k1v4z <addnguyen32gmail.com>
2024-11-26 16:12:30 +0700

    chore: Config tailwind css in webpack

--- src/renderer/app.tsx
+++ src/renderer/app.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import "./styles.css";
 
 const container = document.getElementById("root");
 
 const App = () => {
-  return <h1>Hello World</h1>;
+  return <h1 className="text-red-500 bg-red-400">Hello World</h1>;
 };
 
 const root = createRoot(container!); // createRoot(container!) if you use TypeScript
---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/renderer/*.{ts,tsx,html}',
+    './src/renderer/components/**/*.{ts,tsx,html}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
