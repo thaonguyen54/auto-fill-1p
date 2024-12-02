db8e878e26fac65af9af4a8432434ace22aa7776
k1v4z <addnguyen32gmail.com>
2024-12-02 17:51:07 +0700

    fix: remove handle set locale to dependent function

--- src/renderer/login/_components/combobox-locale.tsx
+++ src/renderer/login/_components/combobox-locale.tsx
@@ -1,29 +1,41 @@
 "use client";
 
 import * as React from "react";
-import { Check } from "lucide-react";
-import { Popover, PopoverContent, PopoverTrigger } from "@components/ui/popover";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@components/ui/popover";
 import { Button } from "@components/ui/button";
 import ChevronIcon from "@components/icon/chevron-icon";
-import { Command, CommandGroup, CommandItem, CommandList } from "@components/ui/command";
+import {
+  Command,
+  CommandGroup,
+  CommandItem,
+  CommandList,
+} from "@components/ui/command";
 
-interface OnePasswordLocaleProps {
-  icon: React.ReactNode;
+type OnePasswordLocale = {
   link: string;
   locale: string;
-}
+};
 
-const ONEPASSWORD_LOCALES: OnePasswordLocaleProps[] = [
-  { icon: <Check />, link: "1Password.ca", locale: "ca" },
-  { icon: <Check />, link: "1Password.eu", locale: "eu" },
-  { icon: <Check />, link: "ent.1Password.com", locale: "en" },
+const ONEPASSWORD_LOCALES: OnePasswordLocale[] = [
+  { link: "1Password.ca", locale: "ca" },
+  { link: "1Password.eu", locale: "eu" },
+  { link: "ent.1Password.com", locale: "en" },
 ];
 
 export function ComboboxLocale() {
   const [open, setOpen] = React.useState(false);
-  const [value, setValue] = React.useState<OnePasswordLocaleProps>(
+  const [value, setValue] = React.useState<OnePasswordLocale>(
     ONEPASSWORD_LOCALES[2]
   );
+
+  const handleSelect = (locale: OnePasswordLocale) => {
+    setValue(locale);
+    setOpen(false);
+  };
 
   return (
     <Popover open={open} onOpenChange={setOpen}>
@@ -46,10 +58,7 @@ export function ComboboxLocale() {
                 <CommandItem
                   key={locale.locale}
                   value={locale.link}
-                  onSelect={() => {
-                    setValue(locale);
-                    setOpen(false);
-                  }}
+                  onSelect={() => handleSelect(locale)}
                 >
                   {locale.link}
                 </CommandItem>
