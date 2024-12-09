5839511859b9b3ae88577a67f22330c50eedbe05
k1v4z <addnguyen32gmail.com>
2024-12-09 15:45:21 +0700

    feat: Implement login

--- src/electron/handler/auth-handler.ts
+++ src/electron/handler/auth-handler.ts
@@ -1,20 +1,51 @@
-import { SharedResources } from "../shared-resource";
+import { spawnSync } from "child_process";
 
-type AuthCredentials = {
-    email: string;
-    password: string;
-    secretKey: string;
-}
-export class AuthHandler extends SharedResources {
+import tokenPublisher from "../publisher/token-publisher";
+import { IObserver } from "../publisher/observer";
+
+import { COMMAND } from "./constants";
+import type { AuthCredentials } from "../type";
+
+class AuthHandler implements IObserver {
+    private token: string;
+    
+    constructor() {
+        this.token = '';
+        tokenPublisher.subcribe(this);
+    }
+
+    update(data: any): void {
+        this.token = data;
+    }
+
     login(authCredentials: AuthCredentials) {
-        const tokenProvider = SharedResources.getTokenProviderInstance();
-        const token = "Token after login sucessful"
-        console.log("Logging in with credentials: ", authCredentials);
-        tokenProvider.setToken(token);
-        return token;
+        const args = [
+            'account',
+            'add',
+            '--address', 'https://my.1password.com/',
+            '--email', authCredentials.email,
+            '--secret-key', authCredentials.secretKey,
+            '--signin',
+            '--raw'
+        ];
+
+        const result = spawnSync(COMMAND, args, {
+            input: `${authCredentials.password}\n`,
+            encoding: 'utf8',
+        });
+
+        if (result.status !== 0) {
+            return {success: false, message: 'Check your credentials or network and try again.'};
+        }else{
+            tokenPublisher.setToken(result.stdout);
+            return {success: true, message: 'Login successful!'};
+        }
     }
 
     signUp() {
         return "Sign up successful!";
     }
 }
+
+const authHandler = new AuthHandler();
+export default authHandler;
--- src/electron/preload.ts
+++ src/electron/preload.ts
@@ -1,6 +1,6 @@
 const { contextBridge, ipcRenderer } = require('electron')
-import { CHANNELS } from "./channel"
 
 contextBridge.exposeInMainWorld('electronAPI', {
-    sendMessage: (message: string, ...args: any[]) => ipcRenderer.invoke(CHANNELS.SEND_MESSAGE, message, args),
+    auth: (channel: string, ...args: any[]) => ipcRenderer.invoke(channel, ...args),
+    vault: (channel: string, ...args: any[]) => ipcRenderer.invoke(channel, ...args),
 })
